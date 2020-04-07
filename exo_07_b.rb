e24f94e22fc986ecbd79070e1748b3489798e459
valentindcs <49322710+valentindcs@users.noreply.github.com>
2020-04-07 21:04:58 +0200

    done

--- exo_07_b.rb
+++ exo_07_b.rb
@@ -2,3 +2,5 @@ puts "Bonjour, c'est quoi ton blase ?"
 print "> "
 user_name = gets.chomp
 puts user_name
+
+#ajoute un chevron (>) 
---
+++ exo_07_c.rb
@@ -0,0 +1,4 @@
+user_name = gets.chomp
+puts user_name
+
+#pas d'indication textuel de quel est ton blase

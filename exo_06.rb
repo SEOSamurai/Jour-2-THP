bc5982707188fd94d2c7e3768e274544e27e43d6
valentindcs <49322710+valentindcs@users.noreply.github.com>
2020-04-07 15:15:10 +0200

    ajout d'une ligne

--- exo_06.rb
+++ exo_06.rb
@@ -3,3 +3,10 @@ number_of_days_worked_per_week = 5
 number_of_weeks_in_THP = 11
 
 puts "Travail : #{number_of_hours_worked_per_day * number_of_days_worked_per_week * number_of_weeks_in_THP}"
+#calcul de 3 données
+
+puts "Et en minutes ça fait : #{number_of_minutes_in_an_hour * number_of_hours_worked_per_day * number_of_days_worked_per_week * number_of_weeks_in_THP}"
+#Travail : 550
+#Traceback (most recent call last):
+#exo_06.rb:8:in `<main>': undefined local variable or method `number_of_minutes_in_an_hour' for main:Object (NameError)
+#Did you mean?  number_of_weeks_in_THP

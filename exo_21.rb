52a4a026814476b2e5558c5471488d5847e1a674
valentindcs <49322710+valentindcs@users.noreply.github.com>
2020-04-07 22:36:39 +0200

    done

---
+++ exo_21.rb
@@ -0,0 +1,19 @@
+puts number = gets.chomp.to_i
+
+hashtag = "#"
+
+espace = ""
+
+number.times do
+	espace = espace + " "
+end
+
+number.times do 
+	puts espace + hashtag
+	hashtag = hashtag+"#"
+	number = number-1
+	espace = ""
+	number.times do 
+		espace = espace + " "
+	end
+end

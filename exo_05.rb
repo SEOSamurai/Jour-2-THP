3cbd2a4adb5a8a64b8faafd701fe5c428ef41120
valentindcs <49322710+valentindcs@users.noreply.github.com>
2020-04-07 15:06:25 +0200

    commentaire chaque ligne

--- exo_05.rb
+++ exo_05.rb
@@ -1,24 +1,25 @@
-puts "On va compter le nombre d'heures de travail à THP"
-puts "Travail : #{10 * 5 * 11}"
-puts "En minutes ça fait : #{10 * 5 * 11 * 60}"
+#écrit du texte puts "On va compter le nombre d'heures de travail à THP"
 
-puts "Et en secondes ?"
+#travail : calcul multiplication puts "Travail : #{10 * 5 * 11}"
+#temps en minutes : calcul multiplicationputs "En minutes ça fait : #{10 * 5 * 11 * 60}"
 
-puts 10 * 5 * 11 * 60 * 60
+#écrit du texte puts "Et en secondes ?"
 
-puts "Est-ce que c'est vrai que 3 + 2 < 5 - 7 ?"
+#donne un résultat : 1980000 puts 10 * 5 * 11 * 60 * 60
 
-puts 3 + 2 < 5 - 7
+#écrit du texte puts "Est-ce que c'est vrai que 3 + 2 < 5 - 7 ?"
 
-puts "Ça fait combien 3 + 2 ? #{3 + 2}"
-puts "Ça fait combien 5 - 7 ? #{5 - 7}"
+# compare deux calculs plus ou moins grand puts 3 + 2 < 5 - 7
 
-puts "Ok, c'est faux alors !"
+#texte avec un calcul donnant 5 puts "Ça fait combien 3 + 2 ? #{3 + 2}"
+#texte avec un calcul donnant -2 puts "Ça fait combien 5 - 7 ? #{5 - 7}"
 
-puts "C'est drôle ça, faisons-en plus :"
+#écrit du texte puts "Ok, c'est faux alors !"
 
-puts "Est-ce que 5 est plus grand que -2 ? #{5 > -2}"
-puts "Est-ce que 5 est supérieur ou égal à -2 ? #{5 >= -2}"
-puts "Est-ce que 5 est inférieur ou égal à -2 ? #{5 <= -2}"
+#écrit du texte puts "C'est drôle ça, faisons-en plus :"
 
-#pour afficher le résultat 
+#compare si c'est plus ou moins grand puts "Est-ce que 5 est plus grand que -2 ? #{5 > -2}"
+#compare si c'est supérieur ou égal grandputs "Est-ce que 5 est supérieur ou égal à -2 ? #{5 >= -2}"
+#compare si c'est inférieur ou égalputs "Est-ce que 5 est inférieur ou égal à -2 ? #{5 <= -2}"
+
+
